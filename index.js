150761481f65e442e0c8e9da42a91e02c427d709
Anchobies <anthonychung97@gmail.com>
2021-05-18 14:08:55 -0400

    commit tree a49c8735d11563ddb09aea1a20d9f3b34cf6fa5f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+var returnFirstTwoDrivers = (someArray) => {
+    const arrayFunc = function() {
+        return someArray.slice(0, 2);
+    };
+
+    return arrayFunc(someArray);
+}
+
+var returnLastTwoDrivers = (someArray) => {
+    const arrayFunc = function() {
+        return someArray.slice(-2);
+    };
+
+    return arrayFunc(someArray);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+var createFareMultiplier = num => {
+    return function(fare) { return fare * num };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, driverCall) => driverCall(drivers);
